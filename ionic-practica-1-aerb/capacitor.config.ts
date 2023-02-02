c2e27c0cf5e94c8a457a25afacecb50d6430a72a
ElPotara <rosalesa913@gmail.com>
2023-02-01 21:06:42 -0600

    Mi Primer Commit

---
+++ ionic-practica-1-aerb/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic Practica 1 AERB',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
