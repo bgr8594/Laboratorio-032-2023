1d1aeff8418f823739af50f3a4fb9f3861a1c894
Joel Leon <joel.leonprz@uanl.edu.mx>
2023-02-13 19:58:38 -0600

    Se agrega Práctica 1
    Se agrega práctica 1 con:
    Se inicializa proyecto de Ionic
    Se agrega página de presupuesto
    Se agrega interface de gasto
    Se agrega service de gasto

---
+++ ionic-basic-Joel-Leon/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-basic-Joel-Leon',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
