d03f5ae814638f83d6bcc6ed8904644513944973
SandovalAguilar <ismael.sandoval.aguilar@gmail.com>
2023-02-22 22:10:24 -0600

    Practica II

--- ionic-clase/src/app/service/gasto.service.ts
+++ ionic-clase/src/app/service/gasto.service.ts
@@ -4,14 +4,12 @@ import { Gasto } from '../interface/gasto';
 @Injectable({
   providedIn: 'root'
 })
-
 // Servicio de gastos para operaciones alta, baja, cambios
 export class GastoService {
 
-  gastos: Gasto[] = [];
+  gastos: Gasto[]=[];
 
   constructor() { }
-
   agregar(gasto: Gasto){
     if(this.gastos.length==0){
       gasto.id = 1;
@@ -23,7 +21,7 @@ export class GastoService {
 
   borrarGasto(gasto: Gasto){
     this.gastos = 
-    this.gastos.filter((gasto) => gasto.id != (gasto.id != null?gasto.id:0));
+    this.gastos.filter(g => g.id!= gasto.id);
   }
 
   getGastos(){
