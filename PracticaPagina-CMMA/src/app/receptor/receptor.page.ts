7ea3864b66e235bfa2d372e6b1d32a3e95ec3376
CMMA04 <carlosmn265@gmail.com>
2023-03-05 22:10:48 -0600

    Practica 2 (Con cosas de la 3)

---
+++ PracticaPagina-CMMA/src/app/receptor/receptor.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receptor',
+  templateUrl: './receptor.page.html',
+  styleUrls: ['./receptor.page.scss'],
+})
+export class ReceptorPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
