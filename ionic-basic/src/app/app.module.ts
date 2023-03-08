3b159800f4524017268aa2e4a3995794342edbad
Monserrat Fernández <xiomistar2003@hotmail.com>
2023-03-08 11:05:13 -0600

    Práctica 5-XMAF
    Creación de un menú flotante desde home y tabs que muestran ciertas páginas.

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FloatMenuComponent } from './componentes/float-menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FloatMenuComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ ionic-basic/src/app/componentes/float-menu.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Menu } from '../interface/menu';
+
+@Component({
+  selector: 'app-float-menu',
+  templateUrl: './float-menu.component.html',
+  styleUrls: ['./float-menu.component.scss'],
+})
+export class FloatMenuComponent implements OnInit {
+
+  datosMenu: Menu[] =[
+    {nombre: 'Home',enlace:'/home',
+    icono:'home-outline'},
+    {nombre: 'Alumnos',enlace:'/alumnos',
+  icono:'school-outline'},
+    {nombre: 'Recetas',enlace:'/receta',
+    icono:'restaurant-outline'},
+    {nombre: 'Presupuesto',enlace:'/presupuesto',
+    icono:'cash-outline'},
+    {nombre: 'Inicio',enlace:'/inicio',
+    icono:'navigate-outline'},
+    {nombre: 'Tabs',enlace:'/tabs',
+    icono:'folder-outline'}
+  ];
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {}
+
+  navegar(link: string){
+    this.router.navigate([link]);
+  }
+
+}
