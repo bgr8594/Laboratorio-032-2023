b95cc64867d2555234fce0d222bcafd98b6ef5a8
Benito <bgr8594@gmail.com>
2023-03-09 20:32:31 -0600

    Practica 06
    Firebase 9, Login, Logout, menu con sesion y cambio de titulo BehaviorSubject

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBXa64HNPCoS0-a4uub_WL0IKJ7II3p5m4",
+    authDomain: "laboratorio-032.firebaseapp.com",
+    projectId: "laboratorio-032",
+    storageBucket: "laboratorio-032.appspot.com",
+    messagingSenderId: "536196670663",
+    appId: "1:536196670663:web:0da2fc1f21039f6bd24ae2"
+  }
 };
 
 /*
