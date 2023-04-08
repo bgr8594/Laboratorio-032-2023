0dbb147789fd1d5731d59febfeca292ee446202e
IanPG789 <ianjmontoyat@gmail.com>
2023-04-07 23:00:11 -0600

    Practica 6 - 2086239

---
+++ ionic-basic/src/app/login/login.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule, ModalController } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+import { ModlaErrorComponent } from '../componentes/modla-error.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule
+  ],
+  declarations: [LoginPage, ModlaErrorComponent]
+})
+export class LoginPageModule { }
