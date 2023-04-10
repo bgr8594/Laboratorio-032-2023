f3afe5969b5386181eaf9c88b447606738db98b9
RolandoMM11 <rolandomorenomuniz@gmail.com>
2023-04-10 13:06:56 -0600

    Práctica 6 -2015771

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyAeIrxF9GHrb0vwmq6Mwsy-AoT_zeew8cQ",
+    authDomain: "laboratorio-032-2b30b.firebaseapp.com",
+    projectId: "laboratorio-032-2b30b",
+    storageBucket: "laboratorio-032-2b30b.appspot.com",
+    messagingSenderId: "402911378666",
+    appId: "1:402911378666:web:797caf8516ecb8b47ceb93"
+  }
 };
 
 /*
