cbc46acc47fba1429c829b71d0ebefcfe9da5c36
Armando-T-T <torrestrevinoarmando@gmail.com>
2023-04-13 17:03:09 -0600

    Practica 07

--- ionic-basic/src/app/login/login.module.ts
+++ ionic-basic/src/app/login/login.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
@@ -14,7 +13,8 @@ import { ModlaErrorComponent } from '../componentes/modla-error.component';
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage,ModlaErrorComponent]
 })
