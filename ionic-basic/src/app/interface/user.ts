984dcf7bfa9b58e934b6056f258d7fe67cd477b8
Armando-T-T <torrestrevinoarmando@gmail.com>
2023-04-12 20:02:20 -0600

    Practica 06

--- ionic-basic/src/app/interface/user.ts
+++ ionic-basic/src/app/interface/user.ts
@@ -1,17 +1,4 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { LoginPage } from './login.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: LoginPage
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class LoginPageRoutingModule {}
+export class User {
+  email: string ='';
+  password: string = '';
+}
