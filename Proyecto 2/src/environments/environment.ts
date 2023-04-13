c4405c95df784df45bb7c3d483959c53e2a734c1
mauls12 <123897163+mauls12@users.noreply.github.com>
2023-04-13 15:56:09 -0600

    practica 7
    practica 7

--- Proyecto 2/src/environments/environment.ts
+++ Proyecto 2/src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig:{
-    apiKey: "AIzaSyBXa64HNPCoS0-a4uub_WL0IKJ7II3p5m4",
-    authDomain: "laboratorio-032.firebaseapp.com",
-    projectId: "laboratorio-032",
-    storageBucket: "laboratorio-032.appspot.com",
-    messagingSenderId: "536196670663",
-    appId: "1:536196670663:web:0da2fc1f21039f6bd24ae2"
+    apiKey: "AIzaSyDSzCpSSkS330JhQGT5xPsXjzr0uOvyejM",
+    authDomain: "laboratorio-app-moviles-83969.firebaseapp.com",
+    projectId: "laboratorio-app-moviles-83969",
+    storageBucket: "laboratorio-app-moviles-83969.appspot.com",
+    messagingSenderId: "1078668012507",
+    appId: "1:1078668012507:web:d52835cf681ba9e1ca55fe",
+    measurementId: "G-4YZ514M4FX"
   }
 };
 
