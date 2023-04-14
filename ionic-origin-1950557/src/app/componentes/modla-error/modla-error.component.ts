26593ba384bcb6045581091cb80beae67ca75972
DarCsr <cesarvzz265@gmail.com>
2023-04-13 19:49:31 -0600

    Practica 06

---
+++ ionic-origin-1950557/src/app/componentes/modla-error/modla-error.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modla-error',
+  templateUrl: './modla-error.component.html',
+  styleUrls: ['./modla-error.component.scss'],
+})
+export class ModlaErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
