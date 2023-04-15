3d5e956bc40b7529d286e2bd43bfa9d87e47df6f
ChrisDestroyer <64537705+ChrisDestroyer@users.noreply.github.com>
2023-04-14 19:16:01 -0500

    Practica 8

--- Practica1-CAMC/src/app/app-routing.module.ts
+++ Practica1-CAMC/src/app/app-routing.module.ts
@@ -56,13 +56,15 @@ const routes: Routes = [
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    path: 'destinos',
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule),
+    canActivate: [AutGuardGuard]
   },
   {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    path: 'destinos',
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
   },
+
 
 ];
 
