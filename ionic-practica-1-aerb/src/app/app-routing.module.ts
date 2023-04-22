b44841d1035cf146c9d9de42b22e0c82b7b7d99c
ElPotara <121736143+ElPotara@users.noreply.github.com>
2023-04-21 19:21:34 -0600

    Revert "Practica 6"
    This reverts commit 5893ad1894a29d9a03ac696b43784d6867dde352.

--- ionic-practica-1-aerb/src/app/app-routing.module.ts
+++ ionic-practica-1-aerb/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AutGuardGuard } from './service/aut-guard.guard';
 
 const routes: Routes = [
   {
@@ -18,33 +17,25 @@ const routes: Routes = [
   },
   {
     path: 'alumnos',
-    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule),
-    canActivate: [AutGuardGuard]
-  },
-  {
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },  {
     path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
     path: 'receptor',
-    loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
   },
   {
     path: 'receta',
-    loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./receta/receta.module').then( m => m.RecetaPageModule)
   },
   {
     path: 'detalle-receta',
-    loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule),
-    canActivate: [AutGuardGuard]
-  },
-  {
+    loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
+  },  {
     path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule),
-    canActivate: [AutGuardGuard]
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
 
 ];
