62665393fc023a133026ce9d2691bc74aa02e041
ChrisDestroyer <64537705+ChrisDestroyer@users.noreply.github.com>
2023-05-19 23:34:31 -0500

    Practica 11

--- Practica1-CAMC/src/environments/environment.ts
+++ Practica1-CAMC/src/environments/environment.ts
@@ -15,7 +15,8 @@ export const environment = {
     storageBucket: "ionicdatabase-1946218.appspot.com",
     messagingSenderId: "247797637691",
     appId: "1:247797637691:web:e23b9ffa2d3dd0f9f4b21e"
-  }
+  },
+  apiKeyMaps :'AIzaSyD6OY6_h55XYjZ6r7slPe4mC72hP8jv0zM'
 };
 
 /*
