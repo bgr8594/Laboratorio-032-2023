d9bad83c1a87cbd76602d6afd8f6663cc9f9ed3b
unknown <rosalesa913@gmail.com>
2023-05-21 19:09:42 -0600

    Practica 11

--- ionic-practica-1-aerb/src/environments/environment.ts
+++ ionic-practica-1-aerb/src/environments/environment.ts
@@ -12,7 +12,8 @@ export const environment = {
     messagingSenderId: "854800753953",
     appId: "1:854800753953:web:653d9341ec597fcb0a8a60",
     measurementId: "G-M05P2SP52V"
-  }
+  },
+  apiKeyMaps :'AIzaSyDlWCVIkc3lrFnOG777LmZ6AQoMgCb3GGc'
 };
 
 /*
