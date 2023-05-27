cad07dd2680dbb78223fc487aacda2460e039984
Pedrotgar <pedrotadeo@live.com.mx>
2023-05-27 13:14:35 -0600

    Práctica 1
    práctica 1

---
+++ src/app/presupuesto/presupuesto.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PresupuestoPageRoutingModule } from './presupuesto-routing.module';
+
+import { PresupuestoPage } from './presupuesto.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PresupuestoPageRoutingModule
+  ],
+  declarations: [PresupuestoPage]
+})
+export class PresupuestoPageModule {}
