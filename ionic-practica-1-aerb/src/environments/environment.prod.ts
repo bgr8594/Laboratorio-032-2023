a8d8549a8330834986474b00c0fdee318d82f28f
unknown <rosalesa913@gmail.com>
2023-05-26 19:50:23 -0600

    Practica 13

--- ionic-practica-1-aerb/src/environments/environment.prod.ts
+++ ionic-practica-1-aerb/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCyl2drspc0ruFgOIXMJA3g0_xhEJXGXB4",
+    authDomain: "lab-032-aerb.firebaseapp.com",
+    projectId: "lab-032-aerb",
+    storageBucket: "lab-032-aerb.appspot.com",
+    messagingSenderId: "854800753953",
+    appId: "1:854800753953:web:653d9341ec597fcb0a8a60",
+    measurementId: "G-M05P2SP52V"
+  },
+  apiKeyMaps :'AIzaSyDlWCVIkc3lrFnOG777LmZ6AQoMgCb3GGc'
 };
